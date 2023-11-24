06cbb7d9b1577d48a9f844d6c1fb0696c02c3608
♡DMC♡ <101607573+TEAM-DMC-444@users.noreply.github.com>
2023-11-24 23:54:54 +0100

    Add files via upload

---
+++ html_444/emalple_1.py
@@ -0,0 +1,6 @@
+#-----[sending Request only use Url]-----------
+from dmcxteam import html_444
+sop=html_444('https://www.facebook.com')
+
+print(sop)# Getting only Html all Elements 
+print(sop.text)# Getting only Html
